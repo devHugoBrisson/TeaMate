7fed54691f3d24df316f65827b58109f0a49c8a4
hbrisson <devhugobrisson@gmail.com>
2016-06-28 17:44:26 +0200

    first commit

---
+++ app/src/main/java/com/hugobrisson/teamate/SignInActivity.java
@@ -0,0 +1,9 @@
+package com.hugobrisson.teamate;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+
+
+}
---
+++ app/src/main/java/com/hugobrisson/teamate/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.hugobrisson.teamate;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    AppCompatTextView mTvTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        mTvTitle = (AppCompatTextView) findViewById(R.id.tv_title);
+        mTvTitle.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+        mTvTitle.setVisibility(View.VISIBLE);
+        assert mTvTitle != null;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
